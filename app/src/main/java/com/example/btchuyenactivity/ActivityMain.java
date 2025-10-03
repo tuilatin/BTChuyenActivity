8d5109d1e112961505c262d15592342ba41aac36
tuilatin <nguyentin1763@gmail.com>
2025-10-03 08:31:55 +0700

    push man hinh login

---
+++ app/src/main/java/com/example/btchuyenactivity/ActivityMain.java
@@ -0,0 +1,12 @@
+package com.example.btchuyenactivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ActivityMain extends Activity {
+    @Override
+    public void onCreate(Bundle saveInstance_144){
+        super.onCreate(saveInstance_144);
+        setContentView(R.layout.login);
+    }
+}
