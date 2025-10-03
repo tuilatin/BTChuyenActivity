a643f29aef61bd9068277ea56679ee7fede207a9
tuilatin <nguyentin1763@gmail.com>
2025-10-03 14:57:01 +0700

    them phan userName bên profile khi login

--- app/src/main/java/com/example/btchuyenactivity/MainActivity.java
+++ app/src/main/java/com/example/btchuyenactivity/MainActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
 
@@ -13,10 +14,13 @@ public class MainActivity extends Activity {
         super.onCreate(saveInstance_144);
         setContentView(R.layout.login);
         Button btnLogin_144 = findViewById(R.id.btnLogin_144);
+        EditText editTextText2_144 = findViewById(R.id.editTextText2);
         btnLogin_144.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v_144) {
+                String userName_144 = editTextText2_144.getText().toString();
                 Intent intent_144 = new Intent(MainActivity.this, ProfileActivity.class);
+                intent_144.putExtra("username", userName_144);
                 startActivity(intent_144);
             }
         });
--- app/src/main/java/com/example/btchuyenactivity/ProfileActivity.java
+++ app/src/main/java/com/example/btchuyenactivity/ProfileActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class ProfileActivity extends Activity {
     @Override
@@ -12,6 +13,11 @@ public class ProfileActivity extends Activity {
         setContentView(R.layout.profile);
 
         ImageButton imgBtnBack_144 = findViewById(R.id.imgBtnBack_144);
+        TextView textView_144 = findViewById(R.id.textView2_144);
+
+        String userName_144 = getIntent().getStringExtra("username");
+        textView_144.setText(userName_144);
+
         imgBtnBack_144.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v_144){
